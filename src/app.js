78436e294542473724688b8356891ce3423d8b4a
Nauel Gómez <ngomez@codiara.com>
2026-04-09 21:18:56 +0200

    Middlewares 1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 import express from 'express';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import morgan from 'morgan';
 
+// En app.js inicializamos SOLO la app de express
 const app = express();
+const appDir = dirname(fileURLToPath(import.meta.url));
+// const appDir = dirname(import.meta.url);
 
-const port = 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(join(appDir, '../public')));
+app.use(morgan('tiny'));
+
+// Custom middleware
+// Para todas las peticiones
+// app.use((req, res, next) => {
+//     console.log(req.url);
+//     console.log('Nueva Petición!');
+
+//     // Un middlware siempre tiene que contestar a la petición o llamar a next();
+//     next();
+// });
 
 app.get('/', (req, res) => {
     res.send('Hello World');
