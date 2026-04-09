f3c88c1f0e7a88c96e46875f906da94f4288d2f3
Nauel Gómez <ngomez@codiara.com>
2026-04-09 22:04:37 +0200

    first render

--- src/routes/pages-routes.js
+++ src/routes/pages-routes.js
@@ -17,15 +17,18 @@ pagesRouter.get('/', (req, res, next) => {
                     <link rel="stylesheet" href="/app.css">
                     <link rel="icon" href="/icon.webp">
                     <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
                     <title>${title}</title>
                 </head>
                 <body style="">
-                    <nav>
+                    <nav class="container">
                         <a href="/">Inicio</a>
                         <a href="/tasks">Lista de tareas (${pendingTasks})</a>
                         <a href="/health">Estado de la aplicación</a>
                     </nav>
                     ${content}
+
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
                 </body>
             </html>
     `);
