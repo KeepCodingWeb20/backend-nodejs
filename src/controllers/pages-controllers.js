22312138a665babd530bf2d0fcd7d3dc95e9fded
Nauel Gómez <ngomez@codiara.com>
2026-04-13 19:52:23 +0200

    Todo 1 resuelto

--- src/controllers/pages-controllers.js
+++ src/controllers/pages-controllers.js
@@ -2,18 +2,9 @@ import { countPendingTasks } from '../data/tasksRepository.js';
 
 export async function homePageController(req, res, next) {
     // TODO: Añade la variable que falta a la vista.
+    const pendingTasks = await countPendingTasks();
     res.render('index.html', {
         title: 'Server HTTP Básico',
+        pendingTasks: pendingTasks,
     });
-    // TODO: Refactorizar
-    return;
-    res.locals.title = 'Server HTTP Básico';
-    res.locals.pendingTasks = await countPendingTasks();
-    res.locals.content = `
-        <h1>Server HTTP basico</h1>
-        <p>Este ejemplo ya respira web SSR: una ruta HTML, una lista HTML y una ruta de health.</p>
-    `;
-    res.locals.html = true; // Defino que el contenido es HTML.
-    next();
-    return;
 }
